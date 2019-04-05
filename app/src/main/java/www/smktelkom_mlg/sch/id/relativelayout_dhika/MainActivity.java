204f28d6e2b165a3e10e08f181a75c5747adf283
andhikapradana <andhika_pradana_26rpl@student.smktelkom-mlg.sch.id>
2019-04-05 14:32:55 +0700

    relativelayout

---
+++ app/src/main/java/www/smktelkom_mlg/sch/id/relativelayout_dhika/MainActivity.java
@@ -0,0 +1,13 @@
+mpackage www.smktelkom_mlg.sch.id.relativelayout_dhika;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
